e3da74989f7af4baded33047f26d834fece16261
MarshmallowShadow <choiboseung@naver.com>
2025-05-29 17:38:29 +0900

    Feat: 첨부파일 추가

--- DDL.sql
+++ DDL.sql
@@ -8,17 +8,33 @@ USE `marsh`;
 
 DROP VIEW IF EXISTS `v_post`;
 DROP VIEW IF EXISTS `v_comment`;
+DROP TABLE IF EXISTS `tr_post_attachment_file`;
 DROP TABLE IF EXISTS `t_comment`;
 DROP TABLE IF EXISTS `t_post`;
 DROP TABLE IF EXISTS `t_refresh_token`;
 DROP TABLE IF EXISTS `t_admin`;
 DROP TABLE IF EXISTS `t_user`;
+DROP TABLE IF EXISTS `t_attachment_file`;
 
 
 -- Create Tables
 
+CREATE TABLE IF NOT EXISTS `t_attachment_file` (
+    `id`           CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '첨부파일 ID',
+    `stored_path`  VARCHAR(255)     NOT NULL COMMENT '저장 경로',
+    `stored_name`  VARCHAR(255)     NOT NULL COMMENT '저장된 첨부파일명',
+    `origin_name`  VARCHAR(255)     NOT NULL COMMENT '원본 첨부파일명',
+    `type`         VARCHAR(255)     NOT NULL COMMENT '첨부파일 타입',
+    `size`         BIGINT UNSIGNED  NOT NULL COMMENT '첨부파일 크기(바이트)',
+    `register_dtm` TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록 일시',
+    `register_user` VARCHAR(100)  NULL COMMENT '등록자',
+    `update_dtm`   TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+    `update_user`  VARCHAR(100)  NULL COMMENT '수정자'
+    ) COMMENT '첨부파일';
+
 CREATE TABLE IF NOT EXISTS `t_user` (
     `id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '사용자 ID',
+    `profile_picture_id` CHAR(36)  NULL COMMENT '프로필 사진 첨부파일 ID',
     `username`       VARCHAR(50)   NULL UNIQUE COMMENT '사용자 이름',
     `email`          VARCHAR(100)  NULL UNIQUE COMMENT '이메일',
     `password`       VARCHAR(255)  NULL COMMENT '비밀번호',
@@ -27,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `t_user` (
     `register_user`  VARCHAR(100)  NULL COMMENT '등록자',
     `update_dtm`     TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
     `update_user`    VARCHAR(100)  NULL COMMENT '수정자'
-    );
+    ) COMMENT '사용자';
 
 CREATE TABLE IF NOT EXISTS `t_admin` (
     `id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '관리자 ID',
@@ -40,8 +56,7 @@ CREATE TABLE IF NOT EXISTS `t_admin` (
     `register_user`  VARCHAR(100)  NULL COMMENT '등록자',
     `update_dtm`     TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
     `update_user`    VARCHAR(100)  NULL COMMENT '수정자'
-    );
-
+    ) COMMENT '관리자';
 
 CREATE TABLE IF NOT EXISTS `t_refresh_token` (
     `id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '토큰 고유 ID',
@@ -54,8 +69,7 @@ CREATE TABLE IF NOT EXISTS `t_refresh_token` (
     `register_user`  VARCHAR(100)  NULL COMMENT '등록자',
     `update_dtm`     TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
     `update_user`    VARCHAR(100)  NULL COMMENT '수정자'
-    );
-
+    ) COMMENT '리프레시 토큰';
 
 CREATE TABLE IF NOT EXISTS `t_post` (
     `id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '게시글 ID',
@@ -67,11 +81,11 @@ CREATE TABLE IF NOT EXISTS `t_post` (
     `register_user`  VARCHAR(100)  NULL COMMENT '등록자',
     `update_dtm`     TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
     `update_user`    VARCHAR(100)  NULL COMMENT '수정자'
-    );
+    ) COMMENT '게시글';
 
 CREATE TABLE IF NOT EXISTS `t_comment` (
     `id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '댓글 ID',
-    `post_id`       CHAR(36)      NOT NULL COMMENT '게시글 ID',
+    `post_id`        CHAR(36)      NOT NULL COMMENT '게시글 ID',
     `user_id`        CHAR(36)      NULL COMMENT '작성자 ID',
     `content`        TEXT          NULL COMMENT '내용',
     `deleted`        ENUM('Y', 'N') NOT NULL COMMENT '삭제 여부',
@@ -79,21 +93,31 @@ CREATE TABLE IF NOT EXISTS `t_comment` (
     `register_user`  VARCHAR(100)  NULL COMMENT '등록자',
     `update_dtm`     TIMESTAMP     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
     `update_user`    VARCHAR(100)  NULL COMMENT '수정자'
-    );
+    ) COMMENT '댓글';
+
+CREATE TABLE IF NOT EXISTS `tr_post_attachment_file` (
+    `attachment_file_id`  CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '첨부파일 ID',
+    `post_id`             CHAR(36)      PRIMARY KEY DEFAULT (UUID()) COMMENT '게시글 ID',
+    ) COMMENT '첨부파일-게시글 관계';
+
 
 -- Foreign Keys
 
+ALTER TABLE `t_user`
+    ADD FOREIGN KEY (`profile_picture_id`) REFERENCES `t_attachment_file`(`id`) ON DELETE SET NULL;
+
 ALTER TABLE `t_post`
-    ADD CONSTRAINT `fk_post_user`
-    FOREIGN KEY (`user_id`) REFERENCES `t_user`(`id`) ON DELETE SET NULL;
+    ADD FOREIGN KEY (`user_id`) REFERENCES `t_user`(`id`) ON DELETE SET NULL;
 
 ALTER TABLE `t_comment`
-    ADD CONSTRAINT `fk_comment_post`
-    FOREIGN KEY (`post_id`) REFERENCES `t_post`(`id`) ON DELETE CASCADE;
+    ADD FOREIGN KEY (`post_id`) REFERENCES `t_post`(`id`) ON DELETE RESTRICT;
 
 ALTER TABLE `t_comment`
-    ADD CONSTRAINT `fk_comment_user`
-    FOREIGN KEY (`user_id`) REFERENCES `t_user`(`id`) ON DELETE SET NULL;
+    ADD FOREIGN KEY (`user_id`) REFERENCES `t_user`(`id`) ON DELETE SET NULL;
+
+ALTER TABLE `tr_post_attachment_file`
+    ADD FOREIGN KEY (`attachment_file_id`) REFERENCES `t_attachment_file`(`id`) ON DELETE RESTRICT,
+    ADD FOREIGN KEY (`post_id`) REFERENCES `t_post`(`id`) ON DELETE RESTRICT;
 
 
 -- Views
--- ERD.md
+++ ERD.md
@@ -3,6 +3,7 @@ erDiagram
 
   t_user {
     CHAR(36) id PK "UUID"
+    CHAR(36) profile_picture_id "UUID"
     VARCHAR(50) username "Unique"
     VARCHAR(100) email "Unique"
     VARCHAR(255) password
@@ -25,12 +26,13 @@ erDiagram
     TIMESTAMP update_dtm
     VARCHAR(100) update_user
   }
-  
+
   t_refresh_token {
     CHAR(36) id PK "UUID"
     ENUM owner_type "USER | ADMIN"
     CHAR(36) owner_id "UUID"
     VARCHAR(512) token
+    TIMESTAMP expires_at
     ENUM revoked "Y | N"
     TIMESTAMP register_dtm
     VARCHAR(100) register_user
@@ -38,7 +40,20 @@ erDiagram
     VARCHAR(100) update_user
   }
 
-  t_board {
+  t_attachment_file {
+    CHAR(36) id PK "UUID"
+    VARCHAR(255) stored_path
+    VARCHAR(255) stored_name
+    VARCHAR(255) origin_name
+    VARCHAR(255) type
+    BIGINT size
+    TIMESTAMP register_dtm
+    VARCHAR(100) register_user
+    TIMESTAMP update_dtm
+    VARCHAR(100) update_user
+  }
+
+  t_post {
     CHAR(36) id PK "UUID"
     CHAR(36) user_id FK
     VARCHAR(150) title
@@ -52,7 +67,7 @@ erDiagram
 
   t_comment {
     CHAR(36) id PK "UUID"
-    CHAR(36) board_id FK
+    CHAR(36) post_id FK
     CHAR(36) user_id FK
     TEXT content
     ENUM deleted "Y | N"
@@ -62,8 +77,15 @@ erDiagram
     VARCHAR(100) update_user
   }
 
-  t_user ||--o{ t_board : "Writes"
+  tr_post_attachment_file {
+    CHAR(36) attachment_file_id FK
+    CHAR(36) post_id FK
+  }
+
+  t_user ||--o{ t_post : "Writes"
   t_user ||--o{ t_comment : "Writes"
-  t_board ||--o{ t_comment : "Contains"
+  t_post ||--o{ t_comment : "Contains"
+  t_post ||--o{ tr_post_attachment_file : "Has"
+  t_attachment_file ||--o{ tr_post_attachment_file : "Attached To"
 
 ```
